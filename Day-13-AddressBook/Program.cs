4117cb47347cb1c2a0e52c9c474aed6b4f9ae3d2
Ramya.U <ramyayadav2016@gmail.com>
2020-11-03 13:47:39 +0530

    [Ramya] Add . Added UC_1

--- Day-13-AddressBook/Program.cs
+++ Day-13-AddressBook/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day_13_AddressBook
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application and also implementation of main class.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcome to Address Book!");
+            Console.WriteLine("Hello, Welcome to Address Book Program");
         }
     }
 }
